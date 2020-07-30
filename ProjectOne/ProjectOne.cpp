a2ff2b3c20a1f41b487b369e797d22099ff0c6af
Andy Twitch <and4twitch@outlook.com>
2020-07-30 21:10:45 +0200

    Add project files.

---
+++ ProjectOne/ProjectOne.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World, take 2\n";
+
+    std::cout << "Hello World, take 3\n";
+
+    int x = 0;
+
+    std::cin >> x;
+
+    std::cout << "Integer read: " << x << std::endl;
+
+}
+
